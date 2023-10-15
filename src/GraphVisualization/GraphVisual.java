7ab8a05077ce3e1af4232f793ca6abb5dee965fd
echen642 <98108241+echen642@users.noreply.github.com>
2023-10-15 14:14:27 -0500

    The graphs are now properly layered.

--- src/GraphVisualization/GraphVisual.java
+++ src/GraphVisualization/GraphVisual.java
@@ -23,24 +23,14 @@ public class GraphVisual {
     private HashMap<Integer, ArrayList<Gene>> genes;
 
     public GraphVisual(CanvasWindow canvas) {
-        // Create a graph visual using the neurons in a network.
-        // Each neuron has a bunch of neurons it's connected to
+        // Adds the GraphicsGroups GraphEdge and GraphNode onto the canvas
+        // Sets up the list to hold all nodes
         this.canvas = canvas;
         allNodes = new ArrayList<GraphNode>();
 
-        // double middleNodeX = canvas.getWidth()/2 - 2*circle_radius;
-        // double middleNodeY = canvas.getHeight()/2;
-
-        // Create the input and output nodes
-
         canvas.add(GraphEdge.edges);
         canvas.add(GraphNode.nodes);
-        // // Create the middle nodes
-        // for (int i = 0; i < totalNodes - numInputNodes - numOutputNodes; i++) {
-        //     allNodes.add(new GraphNode(middleNodeX, middleNodeY, circle_radius, canvas));
-        //     middleNodeY += circle_radius;
-        //     middleNodeX += circle_radius;
-        // }
+
     }
 
     /**
@@ -52,32 +42,46 @@ public class GraphVisual {
         new GraphEdge(node1.getCenter(), node2.getCenter(), weight);
     }
 
-    // /**
-    //  * Uses Neural_Constants and sets up the initial layout 
-    //  * of input neurons and output neurons
-    //  * @param n
-    //  */
-    // private void setup() {
-    //     double canvasHeightSegmentInput = canvas.getHeight()/(Neural_Constants.NUM_OF_INPUTS + 1);
-    //     double canvasHeightSegmentOutput = canvas.getHeight()/(Neural_Constants.NUM_OF_OUTPUTS + 1);
+    /**
+     * Uses Neural_Constants and sets up the initial layout 
+     * of input neurons and output neurons
+     * @param n
+     */
+    private void setup() {
+        // Figure out how many layers there are
+        int maxLayer = 0;
+        HashMap<Integer, Integer> layerOrders = layering_nodes();
+        System.out.println("This is nodeToLayer " + layerOrders);
+        for (Integer i : layerOrders.keySet()) {
+            maxLayer = Math.max(maxLayer, layerOrders.get(i));
+        }
+        maxLayer += 1; // layers were 0-indexed
 
-    //     double x = GraphNode.circle_radius;
-    //     double y = canvasHeightSegmentInput;
+        // Figure out what nodes a layer contains
+        HashMap<Integer, ArrayList<Integer>> layerNodes = new HashMap<>();
+        for (int i = 0; i < maxLayer; i++) {
+            layerNodes.put(i, new ArrayList<Integer>());
+        }
+        for (Integer nodeNumber : layerOrders.keySet()) {
+            layerNodes.get(layerOrders.get(nodeNumber)).add(nodeNumber);
+        }
+        System.out.println("This is layerNodes: " + layerNodes);
 
-    //     for (int inputNodeCount = 0; inputNodeCount < Neural_Constants.NUM_OF_INPUTS; inputNodeCount++) {
-    //         GraphNode inputNode = new GraphNode(x, y, GraphNode.circle_radius, canvas);
-    //         allNodes.add(inputNode);
-    //         y += canvasHeightSegmentInput;
-    //     }
+        double canvasWidthSegment = canvas.getWidth()/(maxLayer*2);
+        double x = canvasWidthSegment;
+        for (int currLayer = 0; currLayer < maxLayer; currLayer++) {
+            double canvasHeightSegment = canvas.getHeight()/(layerNodes.get(currLayer).size() + 1);
 
-    //     x = canvas.getWidth() - GraphNode.circle_radius;
-    //     y = canvasHeightSegmentOutput;
-    //     for (int outputNodeCount = 0; outputNodeCount < Neural_Constants.NUM_OF_OUTPUTS; outputNodeCount++) {
-    //         GraphNode outputNode = new GraphNode(x, y, GraphNode.circle_radius, canvas);
-    //         allNodes.add(outputNode);
-    //         y += canvasHeightSegmentOutput;
-    //     }
-    // }
+            // Put the nodes onto the canvas
+            double y = canvasHeightSegment;
+            for (int nodeNumber : layerNodes.get(currLayer)) {
+                GraphNode node = new GraphNode(x, y, GraphNode.circle_radius, canvas);
+                allNodes.add(node);
+                y += canvasHeightSegment;
+            }
+            x += canvasWidthSegment * 2;
+        }
+    }
 
     /**
      * Draws the connections between 2 nodes
@@ -86,7 +90,7 @@ public class GraphVisual {
         setLineScale();
 
         for (int key : genes.keySet()) {
-            System.out.println("This is my key: " + key);
+            // System.out.println("This is my key: " + key);
             for (Gene gene : genes.get(key)) {
                 new GraphEdge(allNodes.get(key).getCenter(), allNodes.get(gene.END_NODE).getCenter(), gene.weight * GraphEdge.SCALE);
             }
@@ -95,21 +99,29 @@ public class GraphVisual {
 
     public void update(HashMap<Integer, ArrayList<Gene>> genes) {
         this.genes = genes;
-        allNodes.clear();
+        // allNodes.clear();
 
-        GraphEdge.reset();
-        GraphNode.reset();
+        // GraphEdge.reset();
+        // GraphNode.reset();
 
         setup();
+
         connect_nodes();
     }
 
-    private void layering_nodes() {
-        ArrayDeque<Integer> nodeNumbers = new ArrayDeque<Integer>();
+    public HashMap<Integer, Integer> layering_nodes() {
+        // Is a queue structure
+        ArrayList<Integer> nodeNumbers = new ArrayList<Integer>();
         HashMap<Integer, Integer> nodeToLayer= new HashMap<>();
 
-        for (int i = 0; i < Neural_Constants.NUM_OF_INPUTS + Neural_Constants.NUM_OF_OUTPUTS; i++) {
+        // Adding in Input Layer
+        for (int i = 0; i < Neural_Constants.NUM_OF_INPUTS; i++) {
             nodeToLayer.put(i, 0);
+        }
+
+        // Putting output nodes in layer 1
+        for (int i = Neural_Constants.NUM_OF_INPUTS; i < Neural_Constants.NUM_OF_INPUTS + Neural_Constants.NUM_OF_OUTPUTS; i++) {
+            nodeToLayer.put(i, 1);
         }
 
         // Set up Queue
@@ -122,31 +134,29 @@ public class GraphVisual {
                 // Move it to the curr + 1 layer
             // If the node is connected to something in the previous layer
                 // Move it to curr + 1 layer
-        while (true) {
-            nodeNumbers.pop();
+    
+        while (!nodeNumbers.isEmpty()) {
+            Integer currentNode = nodeNumbers.remove(0);
+            // System.out.println("This is the currentNode: " + currentNode);
+            if (genes.containsKey(currentNode)) {
+                genes.get(currentNode).forEach(x -> {
+                    nodeToLayer.put(x.END_NODE, nodeToLayer.get(currentNode) + 1);
+                    nodeNumbers.add(x.END_NODE);
+                });
+            }
         }
 
+        int maxLayer = 1;
+        for (int i = Neural_Constants.NUM_OF_INPUTS; i < Neural_Constants.NUM_OF_INPUTS + Neural_Constants.NUM_OF_OUTPUTS; i++) {
+            maxLayer = Math.max(maxLayer, nodeToLayer.get(i));
+        }
 
+        for (int i = Neural_Constants.NUM_OF_INPUTS; i < Neural_Constants.NUM_OF_INPUTS + Neural_Constants.NUM_OF_OUTPUTS; i++) {
+            nodeToLayer.put(i, maxLayer);
+        }
 
-
-        // while(true) {
-            // int[] lastLayer = layers.get(layers.size()-1).stream().mapToInt(x -> x).toArray();
-            // HashSet<Integer> nextLayer = new HashSet<>();
-            
-            // nodeNumbers.pop();
-            
-
-            // for (int nodeIndex = 0; nodeIndex < lastLayer.length; nodeIndex++) {
-            //     nextLayer.addAll(genes.get(nodeIndex).stream().map(x -> {
-            //         layers.forEach(layer -> {
-            //             layer.removeIf(y -> y == x.END_NODE);
-            //         });
-            //         return x.END_NODE;
-            //     }).toList());
-            // }
-
-            // if (nextLayer.isEmpty()) break;
-        // }
+        // System.out.println(nodeToLayer);
+        return nodeToLayer;
     }
 
     private void setLineScale() {
@@ -165,13 +175,13 @@ public class GraphVisual {
 
         HashMap<Integer, ArrayList<Gene>> genes1 = new HashMap<Integer, ArrayList<Gene>>();
         genes1.put(0, new ArrayList<Gene>(Arrays.asList(new Gene(10, 50))));
-        genes1.put(1, new ArrayList<Gene>(Arrays.asList(new Gene(13, -10))));
-        genes1.put(2, new ArrayList<Gene>(Arrays.asList(new Gene(9, -10))));
-        genes1.put(10, new ArrayList<Gene>(Arrays.asList(new Gene(11, 0))));
+        genes1.put(10, new ArrayList<Gene>(Arrays.asList(new Gene(11, 10))));
         genes1.put(11, new ArrayList<Gene>(Arrays.asList(new Gene(12, -10))));
+        genes1.put(1, new ArrayList<Gene>(Arrays.asList(new Gene(13, -10))));
+        genes1.put(14, new ArrayList<Gene>(Arrays.asList(new Gene(8, -10))));
+        genes1.put(2, new ArrayList<Gene>(Arrays.asList(new Gene(9, -10))));
         genes1.put(12, new ArrayList<Gene>(Arrays.asList(new Gene(13, -10))));
         genes1.put(13, new ArrayList<Gene>(Arrays.asList(new Gene(14, -10))));
-        genes1.put(14, new ArrayList<Gene>(Arrays.asList(new Gene(8, -10))));
 
         CanvasWindow canvas = new CanvasWindow("Testing Graph", GraphVisual_Constants.CANVAS_WIDTH, GraphVisual_Constants.CANVAS_HEIGHT);
         
@@ -179,5 +189,7 @@ public class GraphVisual {
         GraphVisual gv = new GraphVisual(canvas);
         // gv.update(genes);
         gv.update(genes1);
+
+        gv.layering_nodes();
     }
 }
