8190bfde6840a77bf3501cfc56bb5d024a2556d0
Timothylang1 <tlang@macalester.edu>
2023-10-27 11:26:12 -0500

    Beginning visualize constants class

---
+++ src/ConstantFinder/VisualizeConstants.java
@@ -0,0 +1,14 @@
+package ConstantFinder;
+
+import edu.macalester.graphics.CanvasWindow;
+
+public class VisualizeConstants {
+    private CanvasWindow canvas = new CanvasWindow("Constants", 500, 500);
+    private CsvMaker csv = new CsvMaker();
+
+    public VisualizeConstants() {
+        
+    }
+
+
+}
