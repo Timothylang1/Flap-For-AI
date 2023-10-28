464851a8718bcacbd94d111283115e25a3fafcae
Timothylang1 <tlang@macalester.edu>
2023-10-28 00:00:02 -0500

    Finished dataset static visual

--- src/ConstantFinder/DataSet.java
+++ src/ConstantFinder/DataSet.java
@@ -1,5 +1,6 @@
 package ConstantFinder;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -7,15 +8,18 @@ import java.util.List;
 import java.util.Map.Entry;
 
 import NEAT.Neural_Constants;
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Ellipse;
 import edu.macalester.graphics.Line;
 import edu.macalester.graphics.Point;
 
 public class DataSet {
     private ArrayList<HashMap<String, Double>> data;
     public HashMap<String, Line> vectorSet = new HashMap<>();
-    public static final String generation_key = "Average generations";
+    public static final String GENERATION_KEY = "Average generations";
     public static double scale_line;
-    public static double scale_circle;
+    public static double scale_circle_color;
+    public Ellipse final_point = new Ellipse(0, 0, 10, 10);
 
     // Comparator used for sorting data
     private static final Comparator<Entry<String, Double>> compare = new Comparator<Entry<String, Double>>() {
@@ -30,28 +34,36 @@ public class DataSet {
         }
     };
 
-    public DataSet(ArrayList<HashMap<String, Double>> data) {
+    public DataSet(ArrayList<HashMap<String, Double>> data, CanvasWindow canvas) {
         this.data = data;
 
         // Create vectors
         for (String key : VisualizeConstants.keySet) {
             Line vector = new Line(0, 0, 0, 0);
+            vector.setStrokeWidth(3);
             vectorSet.put(key, vector);
+            canvas.add(vector);
         }
+        canvas.add(final_point);
     }
 
-    private void updateInstance(HashMap<String, Double> weights) {
+    public boolean update(int current_number) {
+        // Get the next weight available. If no weight available, then do nothing
+        if (current_number >= data.size()) return false;
+        HashMap<String, Double> weights = data.get(current_number);
+
         // Sort the weights and then update vectors in order of sorted weights. We also remove the generation key because that isn't a parameter
-        List<Entry<String, Double>> sorted_entries = weights.entrySet().stream().filter(x -> !x.getKey().equals(generation_key)).sorted(compare).toList();
+        List<Entry<String, Double>> sorted_entries = weights.entrySet().stream().filter(x -> VisualizeConstants.keySet.contains(x.getKey())).sorted(compare).toList();
         Point previous_location = new Point(VisualizeConstants.CANVAS_SIZE / 2, VisualizeConstants.CANVAS_SIZE / 2);
         for (Entry<String, Double> entry : sorted_entries) {
+
             // Set start position to previous point
             vectorSet.get(entry.getKey()).setStartPosition(previous_location);
 
             // Calculate end point
             Point end_location = new Point(
-                previous_location.getX(), 
-                previous_location.getY());
+                previous_location.getX() + entry.getValue() * Math.cos(Math.PI * 2 / VisualizeConstants.keySet.size() * (VisualizeConstants.keySet.indexOf(entry.getKey()) + 0.5)) * DataSet.scale_line, 
+                previous_location.getY() + entry.getValue() * -Math.sin(Math.PI * 2 / VisualizeConstants.keySet.size() * (VisualizeConstants.keySet.indexOf(entry.getKey()) + 0.5)) * DataSet.scale_line);
             
             vectorSet.get(entry.getKey()).setEndPosition(end_location);
 
@@ -59,21 +71,27 @@ public class DataSet {
             previous_location = end_location;
         }
 
-        // Add in node for end point
+        // Whatever the last location is, we update the final ellipse to have that location and then scale it based on the weight
+        // The darker the color, the better the average generation
+        final_point.setCenter(previous_location);
+        final_point.setFillColor(new Color((int) (255 - weights.get(GENERATION_KEY) * scale_circle_color)));
+
+        // Return true because successfully ran
+        return true;
     }
 
-    public static void main(String[] args) {
-        ArrayList<HashMap<String, Double>> example = new ArrayList<>();
-        HashMap<String, Double> test = new HashMap<>();
-        example.add(test);
-        test.put("Add C", 1.0);
-        test.put("Add N", .3);
-        test.put("Mod Wgt", .05);
-        test.put("Diff", .4);
-        test.put("Avg. W", .05);
-        test.put("Excess", .09);
-        test.put("Average generations", 5.078);
-        DataSet data = new DataSet(example);
-        data.updateInstance(test);
-    }
+    // public static void main(String[] args) {
+    //     ArrayList<HashMap<String, Double>> example = new ArrayList<>();
+    //     HashMap<String, Double> test = new HashMap<>();
+    //     example.add(test);
+    //     test.put("Add C", 1.0);
+    //     test.put("Add N", .3);
+    //     test.put("Mod Wgt", .05);
+    //     test.put("Diff", .4);
+    //     test.put("Avg. W", .05);
+    //     test.put("Excess", .09);
+    //     test.put("Average generations", 5.078);
+    //     DataSet data = new DataSet(example);
+    //     data.update(test);
+    // }
 }
--- src/ConstantFinder/VisualizeConstants.java
+++ src/ConstantFinder/VisualizeConstants.java
@@ -6,44 +6,81 @@ import java.awt.Color;
 
 import edu.macalester.graphics.Arc;
 import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.GraphicsText;
-import edu.macalester.graphics.Line;
+import edu.macalester.graphics.ui.Button;
 
 public class VisualizeConstants {
     public static final int CANVAS_SIZE = 800;
-    private CanvasWindow canvas = new CanvasWindow("Constants", CANVAS_SIZE, CANVAS_SIZE);
+    public static final int CANVAS_WIDTH = 900;
+    private CanvasWindow canvas = new CanvasWindow("Constants", CANVAS_WIDTH, CANVAS_SIZE);
     private CsvMaker csv = new CsvMaker();
     private ArrayList<DataSet> dataHolders = new ArrayList<>();
-    public static ArrayList<String> keySet = new ArrayList<>();
+    public static ArrayList<String> keySet;
+    private int current_run = 0;
+    private GraphicsGroup border = new GraphicsGroup();
 
     /*
      * IMPORTANT: DO NOT SWAP THE ORDER OF THESE METHODS
      */
     public VisualizeConstants(ArrayList<ArrayList<HashMap<String, Double>>> data) {
         // Get all the keys
-        keySet.addAll(data.get(0).get(0).keySet());
-        keySet.remove(DataSet.generation_key);
+        keySet = new ArrayList<>(data.get(0).get(0).keySet());
+        keySet.remove(DataSet.GENERATION_KEY);
+
+        // Add in border
+        canvas.add(border);
 
         // Then create data objects
-        data.forEach(x -> dataHolders.add(new DataSet(x)));
+        data.forEach(x -> dataHolders.add(new DataSet(x, canvas)));
 
-        // Setup scales
-        double scale_line = 0;
-        for (int i = 0; i < keySet.size() / 2; i++) {
-            scale_line += Math.cos(i);
-        }
+        // Add in button objects
+        for (int i = 0; i < keySet.size(); i++) {
+            String variable_name = keySet.get(i);
 
-        DataSet.scale_line = (CANVAS_SIZE - 40) / 2 * scale_line;
+            // Add in text button
+            Button button = new Button(variable_name);
+            button.setPosition(CANVAS_SIZE, i * 30 + 10);
+            button.onClick(() -> {
+                // Set color of vector lines + reset all vector lines
+                dataHolders.forEach(dataHolder -> {
+                    keySet.forEach(key -> {
+                        dataHolder.vectorSet.get(key).setStartPosition(0, 0);
+                        dataHolder.vectorSet.get(key).setEndPosition(0, 0);
+                    });
+                    dataHolder.final_point.setCenter(-20, -20);
+                });
+
+                // Remove from keySet if not in, else add it back in
+                if (keySet.contains(variable_name)) keySet.remove(variable_name);
+                else keySet.add(variable_name);
+
+                // Reset based on new keyset, then update visual
+                reset();
+                update();
+            });
+
+            canvas.add(button);
+        };
+
+        // Setup visual elements + line scale
+        reset();
         
+        // Setup max generation
+        double max_generations = data.stream().mapToDouble(x -> x.stream().mapToDouble(y -> y.get(DataSet.GENERATION_KEY)).max().getAsDouble()).max().getAsDouble();
+        DataSet.scale_circle_color = 255 / max_generations;
 
-        double max_generations = data.stream().mapToDouble(x -> x.stream().mapToDouble(y -> y.get(DataSet.generation_key)).max().getAsDouble()).max().getAsDouble();
-
-
-        // Setup visual elements
-        setup();
+        // Update the visual to hold the inital dataset
+        update();
     }
 
-    private void setup() {
+    private void reset() {
+        // Clear border
+        border.removeAll();
+
+        // Reset current run
+        current_run = 0;
+
         // Initial setup of elements
         for (int i = 0; i < keySet.size(); i++) {
 
@@ -54,10 +91,11 @@ public class VisualizeConstants {
             Arc arc = new Arc(20, 20, CANVAS_SIZE - 40, CANVAS_SIZE - 40, 360.0 / keySet.size() * i, 360.0 / keySet.size());
             arc.setStrokeWidth(10);
             arc.setStrokeColor(color);
-            canvas.add(arc);
+            border.add(arc);
 
             // Text field
-            GraphicsText text = new GraphicsText(keySet.get(i));
+            String variable_name = keySet.get(i);
+            GraphicsText text = new GraphicsText(variable_name);
             text.setFontSize(20);
             double rotation = 90 - 360.0 / keySet.size() * (i + 0.5);
             text.rotateBy((rotation < -90 ? rotation + 180: rotation));
@@ -65,13 +103,30 @@ public class VisualizeConstants {
                 CANVAS_SIZE / 2 + ((CANVAS_SIZE - 90) / 2 * Math.cos(2 * Math.PI / keySet.size() * (i + 0.5))),
                 CANVAS_SIZE / 2 + ((CANVAS_SIZE - 90) / 2 * -Math.sin(2 * Math.PI / keySet.size() * (i + 0.5)))
             );
-            canvas.add(text);
+            border.add(text);
 
-            // Set color of vector lines
+            // Reset the colors of the vector lines
             for (DataSet holder : dataHolders) {
-                holder.vectorSet.get(keySet.get(i)).setStrokeColor(color);
+                holder.vectorSet.get(variable_name).setStrokeColor(color);
             }
         }
+
+        // Setup line_scale
+        double scale_line = 0;
+        double current_angle = (((keySet.size() / 2 + 1) - 2) * 180 - ((keySet.size() - 2) * 180.0 / keySet.size()) * (keySet.size() / 2 - 1)) / 2; // Calculates starting angle
+        for (int i = 0; i < keySet.size() / 2; i++) {
+            scale_line += Math.abs(Math.cos(current_angle * Math.PI / 180));
+            System.out.println(current_angle);
+            current_angle += (keySet.size() - 2) * 180.0 / keySet.size();
+        }
+        DataSet.scale_line = ((CANVAS_SIZE - 40) / 2) / scale_line;
+    }
+
+    private void update() {
+        for (DataSet dataset : dataHolders) {
+            dataset.update(current_run);
+        }
+        current_run += 1;
     }
 
     public static void main(String[] args) {
@@ -80,19 +135,13 @@ public class VisualizeConstants {
         HashMap<String, Double> test = new HashMap<>();
         example.add(test);
         data.add(example);
-        test.put("Add C", 1.0);
-        test.put("Add N", .3);
-        test.put("Mod Wgt", .05);
-        test.put("Diff", .4);
-        test.put("Avg. W", .05);
-        test.put("Excess", .09);
+        test.put("Add C", 0.1);
+        test.put("Add N", 1.0);
+        test.put("Mod Wgt", 0.2);
+        test.put("Diff", 1.0);
+        test.put("Avg. W", 0.0);
+        test.put("Excess", 1.0);
         test.put("Average generations", 5.078);
         new VisualizeConstants(data);
-
-        // CanvasWindow canvas = new CanvasWindow("test", CANVAS_SIZE, CANVAS_SIZE);
-        // Arc arc = new Arc(0, 0, CANVAS_SIZE / 2, CANVAS_SIZE / 2, 90, 90);
-        // arc.setCenter(CANVAS_SIZE / 2, CANVAS_SIZE / 2);
-        // canvas.add(arc);
-
     }
 }
